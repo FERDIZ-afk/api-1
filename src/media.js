41b6316124097c775a97559fe8b6d3284c807e89
Danzz Coding <muhammadramdani196453@gmail.com>
2022-10-31 19:00:45 +0700

    Add files via upload

--- src/media.js
+++ src/media.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch')
 const baseURI = 'https://danzzapi.xyz'
 const apikey = 'danzz' // Get in https://danzzapi.xyz
-const apikeyprem = 'YOUR_APIKEY' // Get in https://danzzapi.xyz
+const apikeyprem = 'danzzprem' // Get in https://danzzapi.xyz
+const apikeyvip = 'danzzvip' // Get in https://danzzapi.xyz
 
 module.exports.ytplay = function ytplay(query) {
 	return new Promise((resolve, reject) => {
@@ -48,7 +49,16 @@ module.exports.ytvideo = function ytvideo(url) {
 	})
 }
 
-module.exports.tiktok = function tiktok(url) {
+module.exports.ttaudio = function ttaudio(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/ttmp3?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.ttvideo = function ttvideo(url) {
 	return new Promise((resolve, reject) => {
 		fetch(baseURI+'/api/downloader/ttmp4?url=' + url +'&apikey=' + apikey)
 		.then(res => res.json())
@@ -57,9 +67,117 @@ module.exports.tiktok = function tiktok(url) {
 	})
 }
 
-module.exports.tiktokaudio = function tiktokaudio(url) {
+module.exports.fbdl = function fbdl(url) {
 	return new Promise((resolve, reject) => {
-		fetch(baseURI+'/api/downloader/ttmp3?url=' + url +'&apikey=' + apikey)
+		fetch(baseURI+'/api/downloader/facebook?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.twdl = function twdl(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/twitter?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.igstory = function igstory(username) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/igstory?username=' + username +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.igreels = function igreels(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/igreels?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.igdl = function igdl(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/igvideo?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.igimg = function igimg(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/igimg?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.snapinsta = function snapinsta(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/snapinsta?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.ssstik = function ssstik(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/ssstik?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.snaptik = function snaptik(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/snaptik?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.sfrom = function sfrom(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/savefrom?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+	
+module.exports.mediafire = function mediafire(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/mediafire?url=' + url +'&apikey=' + apikeyprem)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.zippy = function zippy(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/zippy?url=' + url +'&apikey=' + apikey)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.scloud = function scloud(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/downloader/soundcloud?url=' + url +'&apikey=' + apikey)
 		.then(res => res.json())
 		.then(resolve)
 		.catch(reject)
