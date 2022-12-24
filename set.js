37bd8cd8bac024c561487b22717cd8366a9817a1
Danzz Coding <muhammadramdani196453@gmail.com>
2022-12-24 17:52:33 +0700

    Add files via upload

---
+++ set.js
@@ -0,0 +1,5 @@
+// Setting
+baseURL = 'https://danzzapi.xyz'
+freeKEY = 'YOUR_KEY' // Get in https://danzzapi.xyz
+premKEY = 'YOUR_KEY_PREM' // Get in https://danzzapi.xyz/pricing
+vipKEY = 'YOUR_KEY_VIP' // Get in https://danzzapi.xyz/pricing
